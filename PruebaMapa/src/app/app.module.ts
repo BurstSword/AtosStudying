cb525f231d39a9f4f5b0c9f67054982db753acd3
Miguel <miguelespigaresr@gmail.com>
2021-04-19 10:17:53 +0200

    PruebaMapa

---
+++ PruebaMapa/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OlMapComponent } from './components/ol-map/ol-map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OlMapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
